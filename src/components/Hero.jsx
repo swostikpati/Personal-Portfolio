6f015c4fbfb0ba80c133cf370ba95ca23791808b
Swostik Pati <swostikpati@gmail.com>
2023-05-09 05:29:56 +0400

    small changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,12 +9,12 @@ const Hero = () => {
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         {/* stick and ball on the side */}
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className='w-5 h-5  rounded-full bg-[#915eff]' />
-          <div className='w-1 sm:h-80 h-40  violet-gradient' />
+          <div className='w-5 h-5  rounded-full bg-[#56DDD2]' />
+          <div className='w-1 sm:h-80 h-40  cyan-gradient' />
         </div>
         {/* heaidng and subheading */}
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Swostik</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#56DDD2]'>Swostik</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}> Fill up <br className='sm:block hidden' />later</p>
         </div>
 
